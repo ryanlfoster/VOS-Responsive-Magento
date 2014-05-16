68f01fbbcdc0428242e6a25f09017401635fb4db
p_r_i_m_e <sean@tilbury.org>
2014-05-16 19:01:10 +0100

    add loads of header and background images
    text formatting tweaks,  and logo media query change

---
+++ vos/inc/about-background.php
@@ -0,0 +1,10 @@
+<div id="bg">
+			<span data-picture data-alt="Vikings of Skate">
+			<span data-src="images/backgrounds/2013grad.jpg"></span> 
+			<span data-src="images/backgrounds/2013grad.jpg" data-media="(min-width: 400px)"></span>
+			<span data-src="images/backgrounds/2013grad.jpg" data-media="(min-width: 800px)"></span>
+			<span data-src="images/backgrounds/2013grad.jpg" data-media="(min-width: 1000px)"></span>
+			<!-- Fallback content for non-JS browsers. Same img src as the initial, unqualified source element. -->
+			<noscript><img src="images/backgrounds/2013grad.jpg" alt="Vikings of Skate"></noscript>
+			</span>
+</div>
