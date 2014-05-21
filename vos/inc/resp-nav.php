d9c5ff9ed51acc8f45acfdfca29c96ec9abb515b
p_r_i_m_e <sean@tilbury.org>
2014-05-21 20:06:39 +0100

    jquery fix for magento,

--- vos/inc/resp-nav.php
+++ vos/inc/resp-nav.php
@@ -1,4 +1,4 @@
-<nav><div class="menu-btn"><img src="img/btn-hamburger.png" alt=""></div>
+<nav><div class="menu-btn"><img src="/img/btn-hamburger.png" alt=""></div>
 			<ul class="menu">
   
                   <li class="menu-item"><a class="menu-button" href="#">CLOTHING</a>
@@ -20,17 +20,17 @@
 					         <li class="menu-item"><a href="/this-and-that">THIS&amp;THAT</a></li>
 							 
 							 <li class="vos-social-buttons"><a href="http://www.facebook.com/pages/Vikings-Of-Skate/160887060630674/" target="_blank">
-									<img onmouseover="this.src='img/icons/social/facebook-rollover.png'" height="20" width="20"  onmouseout="this.src='img/icons/social/facebook.png'" border="0" src="img/icons/social/facebook.png"  alt="Facebook" /></a></li>
+									<img onmouseover="this.src='/img/icons/social/facebook-rollover.png'" height="20" width="20"  onmouseout="this.src='/img/icons/social/facebook.png'" border="0" src="/img/icons/social/facebook.png"  alt="Facebook" /></a></li>
 							<li class="vos-social-buttons"><a href="http://twitter.com/vikingsofskate/" target="_blank">
-									<img onmouseover="this.src='img/icons/social/twitter-rollover.png'" height="20" width="20" onmouseout="this.src='img/icons/social/twitter.png'" border="0" src="img/icons/social/twitter.png" alt="Twitter" /></a></li>
+									<img onmouseover="this.src='/img/icons/social/twitter-rollover.png'" height="20" width="20" onmouseout="this.src='/img/icons/social/twitter.png'" border="0" src="/img/icons/social/twitter.png" alt="Twitter" /></a></li>
 							<li class="vos-social-buttons"><a href="http://pinterest.com/vikingsofskate/skateboarding/" target="_blank">
-									<img onmouseover="this.src='img/icons/social/pinterest-rollover.png'"  height="20" width="20" onmouseout="this.src='img/icons/social/pinterest.png'" border="0" src="img/icons/social/pinterest.png" alt="Pinterest" /></a></li>
+									<img onmouseover="this.src='/img/icons/social/pinterest-rollover.png'"  height="20" width="20" onmouseout="this.src='/img/icons/social/pinterest.png'" border="0" src="/img/icons/social/pinterest.png" alt="Pinterest" /></a></li>
 							<li class="vos-social-buttons"><a href="http://instagram.com/vikingsofskate/" target="_blank">
-									<img onmouseover="this.src='img/icons/social/instagram-rollover.png'"  height="20" width="20" onmouseout="this.src='img/icons/social/instagram.png'" border="0" src="img/icons/social/instagram.png" alt="Instagram" /></a></li>
+									<img onmouseover="this.src='/img/icons/social/instagram-rollover.png'"  height="20" width="20" onmouseout="this.src='/img/icons/social/instagram.png'" border="0" src="/img/icons/social/instagram.png" alt="Instagram" /></a></li>
 							<li class="vos-social-buttons"><a href="http://vikingsofskate.tumblr.com/" target="_blank">
-									<img onmouseover="this.src='img/icons/social/tumblr-rollover.png'"  height="20" width="20" onmouseout="this.src='img/icons/social/tumblr.png'" border="0" src="img/icons/social/tumblr.png" alt="Tumblr" /></a></li>		
+									<img onmouseover="this.src='/img/icons/social/tumblr-rollover.png'"  height="20" width="20" onmouseout="this.src='/img/icons/social/tumblr.png'" border="0" src="/img/icons/social/tumblr.png" alt="Tumblr" /></a></li>		
 							<li class="vos-social-buttons">	<a href="https://www.youtube.com/user/vikingsofskate/" target="_blank">
-									<img onmouseover="this.src='img/icons/social/youtube_button_over.png'"  height="19" width="19" onmouseout="this.src='img/icons/social/youtube_button.png'" border="0" src="img/icons/social/youtube_button.png" alt="YouTube" /></a></li>
+									<img onmouseover="this.src='/img/icons/social/youtube_button_over.png'"  height="19" width="19" onmouseout="this.src='/img/icons/social/youtube_button.png'" border="0" src="/img/icons/social/youtube_button.png" alt="YouTube" /></a></li>
 							
 							<li class="cart-button"><a href="/cart">CART/CHECKOUT</a></li>
 			
