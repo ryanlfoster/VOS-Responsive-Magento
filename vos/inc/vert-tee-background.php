20a151bd5198029c7c1ed2ab999d339272cee64a
p_r_i_m_e <sean@tilbury.org>
2014-06-25 11:17:56 +0100

    product templates

---
+++ vos/inc/vert-tee-background.php
@@ -0,0 +1,12 @@
+<div id="bg">
+    <!-- <img id="larget" src=""  class="source-image" width="85%"/> -->
+			<span data-picture data-alt="Vikings of Skate">
+			<span data-src="/img/bg/bg-400-p.jpg"></span> 
+			<span data-src="/img/products/shirts/bg/vert-s" data-media="(min-width: 400px)and (orientation : portrait)"></span>
+            <span data-src="/img/products/shirts/bg/vert-l-s" data-media="(min-width: 400px)and (orientation : landscape)"></span>
+			
+			<span data-src="/img/products/shirts/bg/vert-l.jpg" data-media="(min-width: 1000px)"></span>
+			<!-- Fallback content for non-JS browsers. Same img src as the initial, unqualified source element. -->
+			<noscript><img src="/img/products/shirts/bg/vert-l.jpg" alt="Vikings of Skate"></noscript>
+			</span>
+</div>
