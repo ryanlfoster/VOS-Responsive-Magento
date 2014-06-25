3ddbe0ff2c3dbc979bf06765e2d08cd9cd4a8ce1
p_r_i_m_e <sean@tilbury.org>
2014-06-25 13:31:11 +0100

    more product template adjustements

--- vos/inc/fade-tee-background.php
+++ vos/inc/fade-tee-background.php
@@ -1,12 +1,12 @@
 <div id="bg">
     <!-- <img id="larget" src=""  class="source-image" width="85%"/> -->
 			<span data-picture data-alt="Vikings of Skate">
-			<span data-src="/img/bg/bg-400-p.jpg"></span> 
-			<span data-src="/img/al-air-300.jpg" data-media="(min-width: 400px)and (orientation : portrait)"></span>
-            <span data-src="/img/vosfpmed.jpg" data-media="(min-width: 400px)and (orientation : landscape)"></span>
-			<span data-src="/img/bg/bg-970-p.jpg" data-media="(min-width: 760px)"></span>
-			<span data-src="/img/bg/bg-xxl.jpg" data-media="(min-width: 1000px)"></span>
+			<span data-src="/img/products/shirts/bg/fade-s.jpg"></span> 
+			<span data-src="/img/products/shirts/bg/fade-s.jpg" data-media="(min-width: 400px)and (orientation : portrait)"></span>
+            <span data-src="/img/products/shirts/bg/fade-l-s.jpg" data-media="(min-width: 400px)and (orientation : landscape)"></span>
+			
+			<span data-src="/img/products/shirts/bg/fade-l.jpg" data-media="(min-width: 1000px)"></span>
 			<!-- Fallback content for non-JS browsers. Same img src as the initial, unqualified source element. -->
-			<noscript><img src="/img/bg/bg-low.jpg" alt="Vikings of Skate"></noscript>
+			<noscript><img src="/img/products/shirts/bg/fade-l.jpg" alt="Vikings of Skate"></noscript>
 			</span>
 </div>
--- vos/inc/vert-tee-background.php
+++ vos/inc/vert-tee-background.php
@@ -1,9 +1,9 @@
 <div id="bg">
     <!-- <img id="larget" src=""  class="source-image" width="85%"/> -->
 			<span data-picture data-alt="Vikings of Skate">
-			<span data-src="/img/bg/bg-400-p.jpg"></span> 
-			<span data-src="/img/products/shirts/bg/vert-s" data-media="(min-width: 400px)and (orientation : portrait)"></span>
-            <span data-src="/img/products/shirts/bg/vert-l-s" data-media="(min-width: 400px)and (orientation : landscape)"></span>
+			<span data-src="/img/products/shirts/bg/vert-s.jpg"></span> 
+			<span data-src="/img/products/shirts/bg/vert-s.jpg" data-media="(min-width: 400px)and (orientation : portrait)"></span>
+            <span data-src="/img/products/shirts/bg/vert-l-s.jpg" data-media="(min-width: 400px)and (orientation : landscape)"></span>
 			
 			<span data-src="/img/products/shirts/bg/vert-l.jpg" data-media="(min-width: 1000px)"></span>
 			<!-- Fallback content for non-JS browsers. Same img src as the initial, unqualified source element. -->
