ac73bb058ad005e4b974525772c60bc48abafa3d
p_r_i_m_e <sean@tilbury.org>
2014-06-26 18:01:59 +0100

    thursday

--- vos/inc/broken-tee-background.php
+++ vos/inc/broken-tee-background.php
@@ -1,10 +1,8 @@
 <div id="bg">
-    <!-- <img id="larget" src=""  class="source-image" width="85%"/> -->
 			<span data-picture data-alt="Vikings of Skate">
-			<span data-src="/img/products/shirts/bg/broken-s.jpg"></span> 
-			<span data-src="/img/products/shirts/bg/broken-s.jpg" data-media="(min-width: 400px)and (orientation : portrait)"></span>
-            <span data-src="/img/products/shirts/bg/broken-l-s.jpg" data-media="(min-width: 400px)and (orientation : landscape)"></span>
-			
+			<span data-src="/img/bg/bg-400-p.jpg"></span> 
+			<span data-src="/img/bg/bg-600-p.jpg" data-media="(min-width: 400px)and (orientation : portrait)"></span>
+            <span data-src="/img/bg/bg-800-p.jpg" data-media="(min-width: 400px)and (orientation : landscape)"></span>
 			<span data-src="/img/products/shirts/bg/broken-l.jpg" data-media="(min-width: 1000px)"></span>
 			<!-- Fallback content for non-JS browsers. Same img src as the initial, unqualified source element. -->
 			<noscript><img src="/img/products/shirts/bg/broken-l.jpg" alt="Vikings of Skate"></noscript>
